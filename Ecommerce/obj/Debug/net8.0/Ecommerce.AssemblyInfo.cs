980bfdce43ac7349e835d425c5f2fba4f71a848b
Shihabuzzaman <shihabuzzaman007@gmail.com>
2025-03-19 01:59:54 +0600

    Updated connection string

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc3c7beb388275e22d7b33919a1b8acf6a6f66e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed5317de48a793fb4c502507d3c165bf1fcc760")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
